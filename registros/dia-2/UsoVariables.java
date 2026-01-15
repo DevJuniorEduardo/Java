b8985b134cba00f8b5f7015fd38aa461c8039a4d
EduOrellana <devjunioreduardo@gmail.com>
2026-01-15 08:20:40 -0300

    Quite los comentarios ineccesarios
    quite los /**/ y cambie el nombres de las variables

--- registros/dia-2/UsoVariables.java
+++ registros/dia-2/UsoVariables.java
@@ -1,19 +1,19 @@
 public class UsoVariables {
 	public static void main(String[] args) {
 		// Variable uso simple
-		/* var edad = 17; // No se puede crear la variable y luego inicializarla con var
-		System.out.println(edad);
-		edad = 35; // Se puede cambiar luego el valor pero no lo cambia si se adelanta de la linea donde se pedira */
+		var edad1 = 17; // No se puede crear la variable y luego inicializarla con var
+		System.out.println(edad1);
+		edad1 = 35; // Se puede cambiar luego el valor pero no lo cambia si se adelanta de la linea donde se pedira 
 
 		// Para inicializar la variable pero no se que poner o puede cambiar, se le pone 0 para INT o '' para String
-		/* String nombre = ""; // Se la puede inicializar con conmillas dobles y sin nada dentro
-		nombre = "Juan";
-		System.out.println(nombre); */
+		String nombre1 = ""; // Se la puede inicializar con conmillas dobles y sin nada dentro
+		nombre1 = "Juan";
+		System.out.println(nombre1); 
 
 		// Operadores comunes en Java - Aritmeticos - Logicos - Relacionales - Booleanos
-		int edad = 15;
-		edad++;
-		String nombre = "Juan";
-		System.out.println("El dia de hoy " + nombre + " cumple " + edad + " años");
+		int edad2 = 15;
+		edad2++;
+		String nombre2 = "Joni";
+		System.out.println("El dia de hoy " + nombre2 + " cumple " + edad2 + " años");
 	}
 }
