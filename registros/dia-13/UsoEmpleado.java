8ac604edd1577ad9fbdd5454a261bd72c85f4d2f
DevJuniorEduardo <devjunioreduardo@gmail.com>
2026-02-23 22:41:37 -0300

    Nueva carpeta correspondiente al dia 13 del registro

---
+++ registros/dia-13/UsoEmpleado.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class UsoEmpleado {
+	public static void main(String[] args) {
+		// Construccion de objetos II
+	}
+}
+
+class Empleado { // Mala practica en proyectos Medianos y Grandes
+	public Empleado(String nom, double sue, int age, int mes, int dia) {
+	}
+
+	private String nombre;
+	private double sueldo;
+	private Date altaContrato;
+}
