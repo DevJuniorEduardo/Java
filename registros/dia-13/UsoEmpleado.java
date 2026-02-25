3c40a3ffeeecd349cf6baaefca63561700b30136
DevJuniorEduardo <devjunioreduardo@gmail.com>
2026-02-24 21:54:12 -0300

    Actualizacion del dia 13, UsoEmpleado, abierto a posibles cambios futuros.

--- registros/dia-13/UsoEmpleado.java
+++ registros/dia-13/UsoEmpleado.java
@@ -1,13 +1,34 @@
-import java.util.Date;
+import java.util.*;
 
 public class UsoEmpleado {
 	public static void main(String[] args) {
-		// Construccion de objetos II
-	}
+		// Uso de GregoriaCalendar
 }
 
 class Empleado { // Mala practica en proyectos Medianos y Grandes
 	public Empleado(String nom, double sue, int age, int mes, int dia) {
+		nombre = nom;
+		sueldo = sue;
+		GregoriaCalendar calendario = GregoriaCalendar(age, mes-1, dia);
+
+		altaContrato = calendario.getTime(); // GetTime devuelve un valor date
+	}
+
+	public String getNombre() { // Metodo getter
+		return nombre;
+	}
+
+	public double getSueldo() {
+		return sueldo;
+	}
+
+	public Date getFechaContrato() {
+		return altaContrato;
+	}
+
+	public void subeSueldo(double porcentaje) { // setter o logica de negocio
+		double aumento = sueldo * (porcentaje / 100);
+		sueldo += aumento;
 	}
 
 	private String nombre;
